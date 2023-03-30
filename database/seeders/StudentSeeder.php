95867af94ebe2e330940e4334a10f318b1adabd1
FardanRashidi <102452422+FardanRashidi@users.noreply.github.com>
2023-03-30 22:02:07 +0800

    modified:   database/csv/data.csv 	modified:   database/seeders/StudentSeeder.php 	deleted:    public/student.blade.php

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -4,40 +4,13 @@ namespace Database\Seeders;
 
 use App\Models\Student;
 use Illuminate\Database\Seeder;
- use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\LazyCollection;
 
 class StudentSeeder extends Seeder
 {
     public function run()
     {
-        // LazyCollection::make(function () {
-        //   $handle = fopen(public_path("data.csv"), 'r');
-          
-        //   while (($line = fgetcsv($handle, 4096)) !== false) {
-        //     $dataString = implode(", ", $line);
-        //     $row = explode(';', $dataString);
-        //     yield $row;
-        //   }
-    
-        //   fclose($handle);
-        // })
-        // ->skip(1)
-        // ->chunk(1000)
-        // ->each(function (LazyCollection $chunk) {
-        //   $records = $chunk->map(function ($row) {
-        //     return [
-        //         "name" => $row[0],
-        //         "email" => $row[1],
-        //         "address" => $row[2],
-        //         "study_course" => $row[3],
-        //     ];
-        //   })->toArray();
-          
-        //   DB::table('student_api')->insert($records);
-        // });
-
-
         Student::truncate();
         $csvData = fopen(base_path('database/csv/data.csv'), 'r');
         $transRow = true;
